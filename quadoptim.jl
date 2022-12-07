34e356be472d38f79a8f13b52d92b0bb64bd5d84
Nolan Reilly <nolan.reilly@nyu.edu>
2022-12-07 11:34:49 -0500

    Fixed adaptive integration

--- quadoptim.jl
+++ quadoptim.jl
@@ -8,9 +8,10 @@ using Plots
 function LegendreRoots(k)
     a = zeros(k+1)
     a[end] = 1.0
-    return roots(Legendre(a))
+    return real.(roots(Legendre(a)))
 end
 
+# Compute a set of quadrature weights for a set of quadrature nodes
 function quadWeights(xx)
     k = length(xx)
     V = vander(Legendre,xx,k)
@@ -19,6 +20,7 @@ function quadWeights(xx)
     return [poly[i](1.0) - poly[i](-1.0) for i = 1:k]
 end
 
+# Helper fn. for adaptiveInterp
 function recursiveInterp(f, a, b, k, tol, Vqr, legnodes)
     x = 0.5*(b-a)*legnodes .+ (b+a)/2
     # From Lagrange find Legendre coefficients
@@ -26,28 +28,30 @@ function recursiveInterp(f, a, b, k, tol, Vqr, legnodes)
     # Test stopping condition
     err = sum(alphas[k:2k].^2)
     if err < tol
-        return ([alphas[1:k]], [[a,b]])
+        return ([alphas[1:k]], [a,b])
     else
         midpt = 0.5*(a+b)
-        left = recursiveInterp(f, a, midpt, tol, Vqr, legnodes)
-        right = recursiveInterp(f, midpt, b, tol, Vqr, legnodes)
-        return (cat(left[0], right[0]), cat(left[1], right[1]))
+        left = recursiveInterp(f, a, midpt, k, tol, Vqr, legnodes)
+        right = recursiveInterp(f, midpt, b, k, tol, Vqr, legnodes)
+        return (cat(left[1], right[1],dims=1), cat(left[2], right[2],dims=1))
     end
 end
 
+# Adaptive interpolation
 function adaptiveInterp(f, a, b, k, tol)
     # Construct 2k Legendre nodes x1:x2k on [-1,1]
     legnodes = LegendreRoots(2k)
     # Construct Lagrange interpolating matrix
-    V = vander(Legendre, legnodes, 2k)
+    V = vander(Legendre, legnodes, 2k-1)
     # Pre-factor it
     Vqr = qr(V)
     # Adaptively interpolate
-    coeffs,brackets = recursiveInterp(f,a,b,tol,Vqr,legnodes)
+    coeffs,brackets = recursiveInterp(f,a,b,k,tol,Vqr,legnodes)
     interval_breaks = unique(brackets)
-    return coeffs, brackets, interval_breaks
+    return coeffs, interval_breaks
 end
 
+# Helper fn; bisection search
 function bisect_srch(x,Y)
     n = length(Y)
     lo = 1
@@ -64,16 +68,17 @@ function bisect_srch(x,Y)
     return lo
 end
 
-function interpoly(x, coeffs, interval_breaks)
+# Evaluates an adaptively interpolated function at an array of points xx
+function interpoly(xx, coeffs, interval_breaks)
     nbrac = length(coeffs)
-    n = length(x)
-    interps = [Legendre(coeffs[i][0]) for i = 1:nbrac]
+    n = length(xx)
+    interps = [Legendre(coeffs[i]) for i = 1:nbrac]
     ip = zeros(n)
     for i = 1:n
         x = xx[i]
         lo = bisect_srch(x, interval_breaks)
         a = interval_breaks[lo]
-        b = interval_breaks[hi]
+        b = interval_breaks[lo+1]
         xnorm = (2x-(a+b))/(b-a)
         ip[i] = interps[lo](xnorm)
     end
@@ -102,12 +107,8 @@ a = 1e-3
 b = 3
 k = 16
 tol = 1e-5
-
-coeffs,brackets = recursiveInterp(f,a,b,k,tol,Vqr,legnodes)
-#=
-coeffs, brackets, interval_breaks = adaptiveInterp(f,a,b,k,tol)
+coeffs, interval_breaks = adaptiveInterp(f,a,b,k,tol)
 
 xx = LinRange(a,b,300)
 ip = interpoly(xx, coeffs, interval_breaks)
 plot(xx,ip)
-=#
